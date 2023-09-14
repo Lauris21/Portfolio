d7b6ee2392511990bb342c2f48aeaa5732073dec
Lauris21 <laurasanchez0301@gmail.com>
2023-09-14 15:44:06 +0200

    styled

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -1,14 +1,14 @@
 import { createGlobalStyle, keyframes } from 'styled-components';
 
 const waveAnimation = keyframes`
- 0% { transform: rotate( 0.0deg) }
-   10% { transform: rotate(14.0deg) }  
-   20% { transform: rotate(-8.0deg) }
-   30% { transform: rotate(14.0deg) }
-   40% { transform: rotate(-4.0deg) }
-   50% { transform: rotate(10.0deg) }
-   60% { transform: rotate( 0.0deg) }
-  100% { transform: rotate( 0.0deg) }
+ from { transform: rotate( 0.0deg) }
+ to { transform: rotate(14.0deg) }  
+ /* from { transform: rotate(-8.0deg) }
+ to { transform: rotate(14.0deg) }
+ from { transform: rotate(-4.0deg) }
+ to { transform: rotate(10.0deg) }
+ from { transform: rotate( 0.0deg) }
+ to { transform: rotate( 0.0deg) } */
 `
 
 const GlobalStyle = createGlobalStyle`
